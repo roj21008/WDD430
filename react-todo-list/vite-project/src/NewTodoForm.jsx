2c5248e73d6f413d3a9d55f6009482183bc0e3a8
roj21008 <roj21008@byui.edu>
2024-11-09 17:19:24 -0500

    last updated

--- react-todo-list/vite-project/src/NewTodoForm.jsx
+++ react-todo-list/vite-project/src/NewTodoForm.jsx
@@ -1,26 +1,29 @@
-import{useState} from "react"
-export function NewTodoForm(props) {
-    props.onSubmit
-    const[newItem,setNewItem] =  useState("")
+import { useState } from "react"
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        if (newItem === "") return
-        props.onSubmit(newItem)
-        /**/
-        setNewItem("")
-    }
-    return(
-        <form onSubmit={handleSubmit}  className="new-item-form">
-        <div className="form-eow">
-          <label htmlFor="item">New Item</label><br />
-          <input 
-            value = {newItem} 
-            onChange={e => setNewItem(e.target.value)}
-            type="text" 
-            id="item" />
-        </div>
-        <button className="btn">Add</button>
+export function NewTodoForm({ onSubmit }) {
+  const [newItem, setNewItem] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (newItem === "") return
+
+    onSubmit(newItem)
+
+    setNewItem("")
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input
+          value={newItem}
+          onChange={e => setNewItem(e.target.value)}
+          type="text"
+          id="item"
+        />
+      </div>
+      <button className="btn">Add</button>
     </form>
-    )
+  )
 }
