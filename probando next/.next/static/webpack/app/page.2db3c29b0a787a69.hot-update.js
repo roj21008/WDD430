6a24a3431f2dcf02ee6c0aa978d3dfee0311df3a
roj21008 <roj21008@byui.edu>
2024-11-11 20:02:37 -0500

    lilke-button developed

---
+++ probando next/.next/static/webpack/app/page.2db3c29b0a787a69.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22I%3A%5C%5Cdocumentos%20Luis%20Rojas%5C%5CBYU%5C%5CFULLSTACK%20DEVELOPMENT%5C%5CWDD430%5C%5Cprobando%20next%5C%5Capp%5C%5Clike-button.js%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false!":
+/*!*********************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22I%3A%5C%5Cdocumentos%20Luis%20Rojas%5C%5CBYU%5C%5CFULLSTACK%20DEVELOPMENT%5C%5CWDD430%5C%5Cprobando%20next%5C%5Capp%5C%5Clike-button.js%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false! ***!
+  \*********************************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./app/like-button.js */ \"(app-pages-browser)/./app/like-button.js\"));\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz0lN0IlMjJyZXF1ZXN0JTIyJTNBJTIySSUzQSU1QyU1Q2RvY3VtZW50b3MlMjBMdWlzJTIwUm9qYXMlNUMlNUNCWVUlNUMlNUNGVUxMU1RBQ0slMjBERVZFTE9QTUVOVCU1QyU1Q1dERDQzMCU1QyU1Q3Byb2JhbmRvJTIwbmV4dCU1QyU1Q2FwcCU1QyU1Q2xpa2UtYnV0dG9uLmpzJTIyJTJDJTIyaWRzJTIyJTNBJTVCJTIyZGVmYXVsdCUyMiU1RCU3RCZzZXJ2ZXI9ZmFsc2UhIiwibWFwcGluZ3MiOiJBQUFBLGtLQUFtSyIsInNvdXJjZXMiOlsiIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiLCB3ZWJwYWNrRXhwb3J0czogW1wiZGVmYXVsdFwiXSAqLyBcIkk6XFxcXGRvY3VtZW50b3MgTHVpcyBSb2phc1xcXFxCWVVcXFxcRlVMTFNUQUNLIERFVkVMT1BNRU5UXFxcXFdERDQzMFxcXFxwcm9iYW5kbyBuZXh0XFxcXGFwcFxcXFxsaWtlLWJ1dHRvbi5qc1wiKTtcbiJdLCJuYW1lcyI6W10sImlnbm9yZUxpc3QiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22I%3A%5C%5Cdocumentos%20Luis%20Rojas%5C%5CBYU%5C%5CFULLSTACK%20DEVELOPMENT%5C%5CWDD430%5C%5Cprobando%20next%5C%5Capp%5C%5Clike-button.js%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false!\n"));
+
+/***/ })
+
+});
